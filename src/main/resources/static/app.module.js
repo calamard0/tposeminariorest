64a9360051d1aab573a4969170c48ed37113b9ab
Ivan Addolorato <Coco@Mac-mini-de-Ivan.local>
2016-11-27 21:51:23 -0300

    acomodando un poco la UI

---
+++ src/main/resources/static/app.module.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    angular.module('inscripciones', ['ui.bootstrap', 'ui.sortable']);
+
+})();
---
+++ src/main/resources/static/controllers/inscripcion/seleccion.nivel.controller.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    angular.module('inscripciones')
+        .controller('seleccionNivelController', function ($scope) {
+            
+        });
+})();
