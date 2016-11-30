3d2f25f4b4d9dbffef415aff75a4eb6d81fae542
Ivan Addolorato <Coco@Mac-mini-de-Ivan.local>
2016-11-29 22:21:14 -0300

    algunos cambios en datos adicionales (incompleto)

--- src/main/resources/static/controllers/inscripcion/datos.adicionales.controller.js
+++ src/main/resources/static/controllers/inscripcion/datos.adicionales.controller.js
@@ -8,11 +8,27 @@
             
             var vm = this;
         
-            vm.jardinEstatal = null;
+            vm.jardinEstatal = false;
+            vm.hermanoEnColegio = false;
+            vm.responsableEnColegio = false;
+        
+            vm.cambioDatoAdicional = cambioDatoAdicional;
+        
+            vm.datosAdicionalesOptions = [
+                {value: false, label: 'No'},
+                {value: true, label: 'Si'},
+            ];
         
             $http.get('json/jardines.json').then(function(data){
                 vm.jardines = data.data.jardin;                
             });
+        
+            function cambioDatoAdicional(datoAdicionalName) {
+                vm.jardinEstatal = false;
+                vm.hermanoEnColegio = false;
+                vm.responsableEnColegio = false;
+                vm[datoAdicionalName] = true;
+            }
 
         });
 })();
