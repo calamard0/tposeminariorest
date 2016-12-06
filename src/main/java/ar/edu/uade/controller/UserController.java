e26dd53b50171e70fb43aeb46ceb7f10fb0ad95a
Ezequiel Maranga <ezequiel.maranga@synapse.com>
2016-12-06 20:16:36 -0300

    mejoras login y acceso denegado

--- src/main/java/ar/edu/uade/controller/UserController.java
+++ src/main/java/ar/edu/uade/controller/UserController.java
@@ -10,11 +10,14 @@ public class UserController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
+		
+        if (error != null) {
+        	model.addAttribute("error", "Your username and password is invalid.");
+        }
 
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
+        if (logout != null) {
+        	model.addAttribute("message", "You have been logged out successfully.");
+        }
 
         return "/views/login.html";
     }
